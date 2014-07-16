d9813923714815e65840e6bd2bc5dd49a716d6d9
James <james@coretronicdeMac-mini.local>
2014-07-16 18:00:04 +0800

    init

--- CameraDemo/ViewController.h
+++ CameraDemo/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+- (IBAction)cameraEvent:(id)sender;
 
 @end
--- CameraDemo/ViewController.m
+++ CameraDemo/ViewController.m
@@ -26,4 +26,26 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)cameraEvent:(id)sender {
+    
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        imagePicker.delegate = self;
+        [self presentViewController:imagePicker animated:YES completion:nil];
+    }
+}
+
+-(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    UIImageWriteToSavedPhotosAlbum(image, nil, nil,nil);
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void) imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 @end
