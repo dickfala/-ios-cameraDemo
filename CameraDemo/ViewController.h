574d1398cea929ac210363de0dd712fe93d8fa99
James <james@coretronicdeMac-mini.local>
2014-07-16 17:40:55 +0800

    Initial Commit

---
+++ CameraDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraDemo
+//
+//  Created by James on 2014/7/16.
+//  Copyright (c) 2014年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
